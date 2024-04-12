fae0608026848c7c222fb4d000f018aace6bdaec
Nermin Covic <ncovic1@etf.unsa.ba>
2024-04-13 00:03:06 +0200

    Changed some settings...

--- src/configurations/RealVectorSpaceConfig.cpp
+++ src/configurations/RealVectorSpaceConfig.cpp
@@ -5,4 +5,4 @@
 #include "RealVectorSpaceConfig.h"
 
 size_t RealVectorSpaceConfig::NUM_INTERPOLATION_VALIDITY_CHECKS = 15;
-float RealVectorSpaceConfig::EQUALITY_THRESHOLD                 = 1e-4;
+float RealVectorSpaceConfig::EQUALITY_THRESHOLD                 = 1e-6;
