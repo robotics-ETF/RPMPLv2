f8a7fddd32ccca0aada3795d5293aa143e97ad03
Nermin Covic <ncovic1@etf.unsa.ba>
2024-06-03 22:24:04 +0200

    Step computing is simply done within 'extendSpine' function.

--- include/robots/AbstractRobot.h
+++ include/robots/AbstractRobot.h
@@ -50,10 +50,8 @@ namespace robots
 																	  const std::shared_ptr<base::State> q_init = nullptr) = 0;
 		virtual std::shared_ptr<Eigen::MatrixXf> computeSkeleton(const std::shared_ptr<base::State> q) = 0;
 		virtual std::shared_ptr<Eigen::MatrixXf> computeEnclosingRadii(const std::shared_ptr<base::State> q) = 0;
-		float computeStep(const std::shared_ptr<base::State> q1, const std::shared_ptr<base::State> q2, float d_c, float rho);
-		float computeStep2(const std::shared_ptr<base::State> q1, const std::shared_ptr<base::State> q2, 
-			const std::vector<float> &d_c_profile, const std::vector<float> &rho_profile);
 		virtual bool checkSelfCollision(const std::shared_ptr<base::State> q1, std::shared_ptr<base::State> &q2) = 0;
+		virtual bool checkSelfCollision(const std::shared_ptr<base::State> q) = 0;
 
 	protected:
 		std::string type;
