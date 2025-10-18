a8683d2c318cc1c3de64d14306ed83d85b05c303
Nermin Covic <ncovic1@etf.unsa.ba>
2025-10-18 20:03:39 +0200

    'AbstractTrajectory' class is created.

---
+++ include/planners/trajectory/AbstractTrajectory.h
@@ -0,0 +1,84 @@
+//
+// Created by nermin on 17.10.25.
+//
+
+#ifndef RPMPL_ABSTRACTTRAJECTORY_H
+#define RPMPL_ABSTRACTTRAJECTORY_H
+
+#include "StateSpace.h"
+#include "TrajectoryConfig.h"
+#include "RealVectorSpaceConfig.h"
+
+namespace planning::trajectory
+{
+    class State
+    {
+    public:
+        State() {}
+        State(size_t num_DOFs);
+        State(const Eigen::VectorXf &pos_);
+        State(const Eigen::VectorXf &pos_, const Eigen::VectorXf &vel_, const Eigen::VectorXf &acc_);
+
+        Eigen::VectorXf pos;
+        Eigen::VectorXf vel;
+        Eigen::VectorXf acc;
+    };
+
+    class AbstractTrajectory
+    {
+    public:
+        AbstractTrajectory(const std::shared_ptr<base::StateSpace> &ss_);
+        AbstractTrajectory(const std::shared_ptr<base::StateSpace> &ss_, planning::trajectory::State current_, float max_iter_time_);
+        virtual ~AbstractTrajectory() = 0;
+        
+        virtual bool computeRegular(planning::trajectory::State current_, planning::trajectory::State target_, 
+                                    float t_iter_remain, float t_max, bool non_zero_final_vel) = 0;
+        virtual bool computeSafe(planning::trajectory::State current_, planning::trajectory::State target_, 
+                                 float t_iter_remain, float t_max, const std::shared_ptr<base::State> q_current) = 0;
+
+        virtual Eigen::VectorXf getPosition(float t) = 0;
+        virtual Eigen::VectorXf getVelocity(float t) = 0;
+        virtual Eigen::VectorXf getAcceleration(float t) = 0;
+
+        float getTimeCurrent(bool measure_time = false);
+        inline float getTimeBegin() const { return time_begin; }
+        inline float getTimeEnd() const { return time_end; }
+        inline float getTimeFinal() const { return time_final; }
+        inline bool getIsZeroFinalVel() const { return is_zero_final_vel; }
+        inline const std::vector<Eigen::VectorXf> &getTrajPointCurrentIter() const { return traj_points_current_iter; }
+
+        void setTimeStart(float time_start_offset_);
+        inline void setTimeBegin(float time_begin_) { time_begin = time_begin_; }
+        inline void setTimeEnd(float time_end_) { time_end = time_end_; }
+        inline void setTimeCurrent(float time_current_) { time_current = time_current_; }
+        inline void setMaxRemainingIterTime(float max_remaining_iter_time_) { max_remaining_iter_time = max_remaining_iter_time_; }
+        
+        bool isFinalConf(const Eigen::VectorXf &pos);
+        void addTrajPointCurrentIter(const Eigen::VectorXf &pos);
+        void clearTrajPointCurrentIter();
+        void recordTrajectory(bool traj_computed);
+        
+    protected:
+        void setParams();
+        
+        std::shared_ptr<base::StateSpace> ss;                       // State space of the robot
+        planning::trajectory::State current;                        // Current robot's state
+        planning::trajectory::State target;                         // Target robot's state
+        float max_iter_time;                                        // Maximal iteration time
+        float max_remaining_iter_time;                              // Maximal remaining iteration time in [s] till the end of the current iteration
+        float time_current;                                         // Elapsed time in [s] from a time instant when the trajectory is created. It is used to determine a current robot's position, velocity and acceleration. 
+        float time_final;                                           // Final time for the trajectory
+        float time_begin;                                           // Time instance in [s] when the trajectory begins in the current iteration
+        float time_end;                                             // Time instance in [s] when the trajectory ends in the current iteration
+        std::chrono::steady_clock::time_point time_start;           // Start time point when the trajectory is created
+        float time_start_offset;                                    // Time offset in [s] which determines how much earlier 'time_start' is created
+        bool is_zero_final_vel;                                     // Whether final velocity is zero. If not, robot will move at constant velocity after 'time_final'.
+        bool all_robot_vel_same;                                    // Whether all joint velocities are the same
+        float max_obs_vel;                                          // Maximal velocity of dynamic obstacles used to generate dynamic bubbles
+        size_t max_num_iter_trajectory;                             // Maximal number of iterations when computing trajectory
+        std::vector<Eigen::VectorXf> traj_points_current_iter;      // Trajectory points from the current iteration to be validated within 'MotionValidity'
+        
+    };
+}
+
+#endif //RPMPL_ABSTRACTTRAJECTORY_H
---
+++ src/planners/trajectory/AbstractTrajectory.cpp
@@ -0,0 +1,150 @@
+#include "AbstractTrajectory.h"
+
+planning::trajectory::State::State(size_t num_DOFs)
+{
+    pos = Eigen::VectorXf::Zero(num_DOFs);
+    vel = Eigen::VectorXf::Zero(num_DOFs);
+    acc = Eigen::VectorXf::Zero(num_DOFs);
+}
+
+planning::trajectory::State::State(const Eigen::VectorXf &pos_)
+{
+    pos = pos_;
+    vel = Eigen::VectorXf::Zero(pos.size());
+    acc = Eigen::VectorXf::Zero(pos.size());
+}
+
+planning::trajectory::State::State(const Eigen::VectorXf &pos_, const Eigen::VectorXf &vel_, const Eigen::VectorXf &acc_)
+{
+    pos = pos_;
+    vel = vel_;
+    acc = acc_;
+}
+
+planning::trajectory::AbstractTrajectory::AbstractTrajectory(const std::shared_ptr<base::StateSpace> &ss_)
+{
+    ss = ss_;
+    current = planning::trajectory::State(ss->num_dimensions);
+    target = current;
+    
+    max_iter_time = 0;
+    max_remaining_iter_time = INFINITY;
+    max_obs_vel = 0;
+
+    setParams();
+}
+
+planning::trajectory::AbstractTrajectory::AbstractTrajectory
+    (const std::shared_ptr<base::StateSpace> &ss_, planning::trajectory::State current_, float max_iter_time_)
+{
+    ss = ss_;
+    current = current_;
+    target = current;
+
+    max_iter_time = max_iter_time_;
+    max_remaining_iter_time = 0;
+    time_current = 0;
+    time_final = 0;
+    time_begin = 0;
+    time_end = 0;
+    time_start = std::chrono::steady_clock::now();
+    time_start_offset = 0;
+    is_zero_final_vel = true;
+
+    max_obs_vel = 0;
+    for (size_t i = 0; i < ss->env->getNumObjects(); i++)
+    {
+        if (ss->env->getObject(i)->getMaxVel() > max_obs_vel)
+            max_obs_vel = ss->env->getObject(i)->getMaxVel();
+    }
+
+    setParams();
+}
+
+planning::trajectory::AbstractTrajectory::~AbstractTrajectory() {}
+
+void planning::trajectory::AbstractTrajectory::setParams()
+{
+    traj_points_current_iter = {};
+    
+    all_robot_vel_same = true;
+    for (size_t i = 1; i < ss->num_dimensions; i++)
+    {
+        if (std::abs(ss->robot->getMaxVel(i) - ss->robot->getMaxVel(i-1)) > RealVectorSpaceConfig::EQUALITY_THRESHOLD)
+        {
+            all_robot_vel_same = false;
+            break;
+        }
+    }
+
+    max_num_iter_trajectory = all_robot_vel_same ? 
+        std::ceil(std::log2(2 * ss->robot->getMaxVel(0) / TrajectoryConfig::FINAL_VELOCITY_STEP)) :
+        std::ceil(std::log2(2 * ss->robot->getMaxVel().maxCoeff() / TrajectoryConfig::FINAL_VELOCITY_STEP));
+}
+
+/// @brief Get current time of the trajectory.
+/// @param measure_time If true, current time will be automatically computed/measured (default: false).
+/// @note 'measure_time' should always be false when simulation pacing is used, since then a time measuring will not be correct! 
+/// In such case, it is assumed that user was previously set 'measure_time' to a correct value.
+float planning::trajectory::AbstractTrajectory::getTimeCurrent(bool measure_time)
+{
+    if (!measure_time)
+        return time_current;
+    
+    time_current = std::chrono::duration_cast<std::chrono::nanoseconds>
+                   (std::chrono::steady_clock::now() - time_start).count() * 1e-9 - time_start_offset;
+    return time_current;
+}
+
+void planning::trajectory::AbstractTrajectory::setTimeStart(float time_start_offset_)
+{
+    time_start = std::chrono::steady_clock::now();
+    time_start_offset = time_start_offset_;
+}
+
+/// @brief Check whether 'q' is a final configuration of the trajectory.
+/// @param pos Configuration to be checked.
+/// @return True if yes, false if not.
+bool planning::trajectory::AbstractTrajectory::isFinalConf(const Eigen::VectorXf &pos)
+{
+    return ((pos - getPosition(time_final)).norm() < RealVectorSpaceConfig::EQUALITY_THRESHOLD) ? true : false;
+}
+
+void planning::trajectory::AbstractTrajectory::addTrajPointCurrentIter(const Eigen::VectorXf &pos)
+{
+    traj_points_current_iter.emplace_back(pos);
+}
+
+void planning::trajectory::AbstractTrajectory::clearTrajPointCurrentIter()
+{
+    traj_points_current_iter.clear();
+}
+
+// This function is just for debugging. It operates in real-time by logging 'spline_current' and 'spline'. 
+// You can hardcode a desired output path for the file to be saved.
+void planning::trajectory::AbstractTrajectory::recordTrajectory(bool traj_computed)
+{
+    std::ofstream output_file {};
+    output_file.open("/home/spear/xarm6-etf-lab/src/etf_modules/RPMPLv2/data/planar_2dof/scenario_random_obstacles/temp/visualize_trajectory.log", 
+        std::ofstream::app);
+    
+    output_file << "Current pos. & target pos.: \n";
+    if (traj_computed)
+    {
+        output_file << current.pos.transpose() << "\n";
+        output_file << target.pos.transpose() << "\n";
+    }
+    else
+        output_file << INFINITY << "\n";
+
+    output_file << "All points: \n";
+    for (float t = getTimeCurrent(); t < getTimeFinal(); t += 0.001)
+        output_file << getPosition(t).transpose() << "\n";
+    output_file << getPosition(getTimeFinal()).transpose() << "\n";
+
+    output_file << "Only points until the end of the current iteration: \n";
+    for (float t = getTimeCurrent(); t < getTimeEnd(); t += 0.001)
+        output_file << getPosition(t).transpose() << "\n";
+    output_file << getPosition(getTimeEnd()).transpose() << "\n";
+    output_file << "--------------------------------------------------------------------\n";
+}
